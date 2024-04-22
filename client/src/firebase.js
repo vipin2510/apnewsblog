f239a0f5c90f475044f895a2d2357a25910f60e7
vipin kumar gautam <vipinkumargautam@vipins.lan>
2024-04-22 20:55:17 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+apiKey: "AIzaSyDW9q8Sa774sIDA7GL55za13rxLQtq7R0I",
+authDomain: "chodingwizards.firebaseapp.com",
+projectId: "chodingwizards",
+storageBucket: "gs://chodingwizards.appspot.com",
+messagingSenderId: "22483401860",
+appId: "1:22483401860:web:d0434a40769f6a326996d1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+//te\
